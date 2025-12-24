e777ca600defdb449c289669fc55c5201b9a06f2
Graham P <graeme@graemep.me>
2025-12-24 16:21:12 +0400

    Add Claude fixes to the build.

--- craco.config.js
+++ craco.config.js
@@ -2,7 +2,18 @@ const CracoAntDesignPlugin = require("craco-antd");
 const webpack = require("webpack")
 
 module.exports = {
-  plugins: [{ plugin: CracoAntDesignPlugin }],
+  plugins: [
+    {
+      plugin: CracoAntDesignPlugin,
+      options: {
+        lessLoaderOptions: {
+          lessOptions: {
+            javascriptEnabled: true,
+          },
+        },
+      },
+    },
+  ],
   webpack: {
     plugins: [
       new webpack.DefinePlugin({
